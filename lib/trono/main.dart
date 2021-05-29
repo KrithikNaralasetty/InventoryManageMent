0dbb3ae9c98d2aed16b446945c3f3f3b75dc5794
Preetham1239 <venkatkrishna343434@gmail.com>
2021-05-29 13:24:20 +0530

    Overflows Removed from Template page

--- lib/trono/main.dart
+++ lib/trono/main.dart
@@ -72,24 +72,33 @@ class _MyHomePageState extends State<MyHomePage> {
           return NetworkGiffyDialog(
             title: Text('use to add textual info, numeric values',
                 style: TextStyle(fontSize: 10.0, fontWeight: FontWeight.w400)),
-            image: Column(
+            image: ListView(
               children: <Widget>[
-                Image.network(
-                  "https://miro.medium.com/max/3675/1*PrOpoN6DYcyTZ6uYIQGoLg.gif",
-                  fit: BoxFit.cover,
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    "Add Title For Text Field",
-                    style:
-                        TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+                Expanded(
+                  flex: 1,
+                  child: Image.network(
+                    "https://miro.medium.com/max/3675/1*PrOpoN6DYcyTZ6uYIQGoLg.gif",
+                    fit: BoxFit.cover,
                   ),
                 ),
-                TextField(
-                  controller: temp,
-                  decoration:
-                      InputDecoration(hintText: "how many litres petrol?"),
+                Expanded(
+                  flex: 1,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      "Add Title For Text Field",
+                      style:
+                          TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: TextField(
+                    controller: temp,
+                    decoration:
+                        InputDecoration(hintText: "how many litres petrol?"),
+                  ),
                 )
               ],
             ),
@@ -138,25 +147,34 @@ class _MyHomePageState extends State<MyHomePage> {
           return NetworkGiffyDialog(
             title: Text(' use as a single option where you can turn on or off.',
                 style: TextStyle(fontSize: 10.0, fontWeight: FontWeight.w400)),
-            image: Column(
+            image: ListView(
               children: <Widget>[
-                Image.network(
-                  "https://i1.wp.com/codemyui.com/wp-content/uploads/2016/04/rounded-toggle-switch.gif?fit=880%2C440&ssl=1",
-                  fit: BoxFit.cover,
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 8.0, left: 8.0, right: 8.0),
-                  child: Text(
-                    "Add Title For Switch",
-                    style:
-                        TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+                Expanded(
+                  flex: 1,
+                  child: Image.network(
+                    "https://i1.wp.com/codemyui.com/wp-content/uploads/2016/04/rounded-toggle-switch.gif?fit=880%2C440&ssl=1",
+                    fit: BoxFit.cover,
                   ),
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 4.0),
-                  child: TextField(
-                    controller: temp,
-                    decoration: InputDecoration(hintText: "Is Car Working"),
+                Expanded(
+                  flex: 1,
+                  child: Padding(
+                    padding: EdgeInsets.only(top: 8.0, left: 8.0, right: 8.0),
+                    child: Text(
+                      "Add Title For Switch",
+                      style:
+                          TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 4.0),
+                    child: TextField(
+                      controller: temp,
+                      decoration: InputDecoration(hintText: "Is Car Working"),
+                    ),
                   ),
                 )
               ],
@@ -210,63 +228,75 @@ class _MyHomePageState extends State<MyHomePage> {
               content: Container(
                 width: MediaQuery.of(context).size.width * 0.4,
                 height: MediaQuery.of(context).size.height * 0.5,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
+                child: ListView(
+                  //mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
-                    Text(descc,
-                        style: TextStyle(
-                            fontSize: 10.0, fontWeight: FontWeight.w400)),
-                    Image.network(
-                      "https://cdn.dribbble.com/users/108183/screenshots/2398414/checkboxes.gif",
-                      fit: BoxFit.cover,
+                    Expanded(
+                      flex: 1,
+                      child: Text(descc,
+                          style: TextStyle(
+                              fontSize: 10.0, fontWeight: FontWeight.w400)),
                     ),
-                    Container(
-                      width: 200.0,
-                      child: TextField(
-                        controller: temp,
-                        decoration: InputDecoration(
-                            hintText: "Title of CheckButton List"),
+                    Expanded(
+                      flex: 1,
+                      child: Image.network(
+                        "https://cdn.dribbble.com/users/108183/screenshots/2398414/checkboxes.gif",
+                        fit: BoxFit.cover,
                       ),
                     ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        Container(
-                          width: 100.0,
-                          child: TextField(
-                            controller: field,
-                            decoration: InputDecoration(hintText: "Item name"),
-                          ),
+                    Expanded(
+                      flex: 1,
+                      child: Container(
+                        width: 200.0,
+                        child: TextField(
+                          controller: temp,
+                          decoration: InputDecoration(
+                              hintText: "Title of CheckButton List"),
                         ),
-                        ElevatedButton(
-                            style: ButtonStyle(
-                                backgroundColor: MaterialStateProperty.all(
-                                    Color.fromARGB(250, 18, 70, 255))),
-                            child: Text(
-                              "Add item",
-                              style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                    Expanded(
+                      flex : 1,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Container(
+                            width: 100.0,
+                            child: TextField(
+                              controller: field,
+                              decoration: InputDecoration(hintText: "Item name"),
                             ),
-                            onPressed: () {
-                              mappeditems.add({
-                                'title': field.text,
-                                'value': false,
-                              });
+                          ),
+                          ElevatedButton(
+                              style: ButtonStyle(
+                                  backgroundColor: MaterialStateProperty.all(
+                                      Color.fromARGB(250, 18, 70, 255))),
+                              child: Text(
+                                "Add item",
+                                style: TextStyle(color: Colors.white),
+                              ),
+                              onPressed: () {
+                                mappeditems.add({
+                                  'title': field.text,
+                                  'value': false,
+                                });
 
-                              setState(() {
-                                items.add(Row(children: <Widget>[
-                                  Container(
-                                    width: 200.0,
-                                    child: CheckboxListTile(
-                                      onChanged: (check) {},
-                                      title: new Text(field.text),
-                                      value: false,
-                                    ),
-                                  )
-                                ]));
-                                field.clear();
-                              });
-                            })
-                      ],
+                                setState(() {
+                                  items.add(Row(children: <Widget>[
+                                    Container(
+                                      width: 200.0,
+                                      child: CheckboxListTile(
+                                        onChanged: (check) {},
+                                        title: new Text(field.text),
+                                        value: false,
+                                      ),
+                                    )
+                                  ]));
+                                  field.clear();
+                                });
+                              })
+                        ],
+                      ),
                     ),
                     Expanded(
                       child: ListView.builder(
